2b2d9fdf2f2c6274b3e21791d7d9d1353aaab072
chenweijie <chenweijie@sunlands.com>
2018-10-31 10:24:57 +0800

    volatile关键字使用

---
+++ src/main/java/cn/chen/exercise/chapter12/VolatileTest.java
@@ -0,0 +1,42 @@
+package cn.chen.exercise.chapter12;
+
+/**
+ * Volatile 不可以保证原子性的例子
+ *
+ * @author Chen WeiJie
+ * @date 2018-10-31 09:18:32
+ **/
+public class VolatileTest {
+    /**
+     * volatile变量自增运算测试
+     *
+     * @author zzm
+     */
+    public static volatile int race = 0;
+
+    public static void increase(){
+        race++;
+    }
+
+    private static final int THREADS_COUNT = 20;
+
+    public static void main(String[] args) {
+        Thread[] threads = new Thread[THREADS_COUNT];
+        for (int i = 0; i < THREADS_COUNT; i++) {
+            threads[i] = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for (int i = 0; i < 10000; i++) {
+                        increase();
+                    }
+                }
+            });
+            threads[i].start();
+        }
+        //等待所有累加线程都结束
+        while (Thread.activeCount() > 1) {
+            Thread.yield();
+        }
+        System.out.println(race);
+    }
+}
